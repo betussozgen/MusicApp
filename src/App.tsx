87bb918bd9e57eb4198d1f81784f206fde5f79f6
betussozgen <fatmabetulozgen@hotmail.com>
2023-03-27 00:06:29 +0300

    add filter

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,52 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
   Image,
   FlatList,
 } from 'react-native';
 import music_data from './music-data.json';
 import SongCard from "./components/SongCard";
+import SearchBar from './components/SearchBar';
+
 
 function App(){
 
+  const [list, setList] = useState(music_data);
+
   const renderSong = ({item}:{item: any})=> <SongCard song={item}/>
   const renderSeperator = () => <View style={styles.seperator}></View>
-  
+  const handleSearch = ({text}:{text: any})  => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+       return currentTitle.indexOf(searchedText) > -1;
+    });
+
+    setList(filteredList);
+  };
  
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
+        <SearchBar onSearch = {handleSearch}/>
         <FlatList
         keyExtractor={(item) => item.id}
-        data={music_data}
+        data={list}
         renderItem={renderSong}
         ItemSeparatorComponent={renderSeperator}
-        
         />
-
-        
       </View>
-      
     </SafeAreaView>
   );
 }
-
-
 export default App;
 
 const styles = StyleSheet.create({
